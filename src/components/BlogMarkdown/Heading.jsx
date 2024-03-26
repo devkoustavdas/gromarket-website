adf35a60115dabc4b899a75ffa9ce661af26c3fd
Koustav Das <teamdecodeofficial@gmail.com>
2024-03-26 21:37:58 +0530

    changed heading.jsx

--- src/components/BlogMarkdown/Heading.jsx
+++ src/components/BlogMarkdown/Heading.jsx
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 const Heading = ({ title, date, tag }) => {
 
   return (
-    <header className="text-white bg-teal-500 mb-4 drop-shadow-md pl-6 pr-7 pt-5 pb-1" title={`GroMarket: ${title}`} aria-label={`GroMarket: ${title}`} style={{
-      borderRadius: "0% 10% 0% 0% / 0% 42% 0% 0%",
-    }}>
-      <h1 className="text-3xl md:text-4xl text-left text-shadow font-sans">
+    <header className="text-slate-50 onesideroundedbox bg-teal-500 mb-4 drop-shadow-md pl-6 pr-7 pt-5 pb-1" title={`GroMarket: ${title}`} aria-label={`GroMarket: ${title}`}>
+      <h1 className="text-2xl md:text-4xl text-left text-shadow font-sans mb-2">
         {title}
       </h1>
       <div>
-        <span>Tags</span><span className='px-1'>&#x2022;</span><div className="mt-2 inline-block whitespace-nowrap">
+        <span>Tags</span>
+        <span className='px-1'>&#x2022;</span>
+        <div className="whitespace-nowrap flex flex-wrap">
           {tag.map((tagItem, index) => (
-            <span key={index} className="bg-teal-600 px-2 mx-1 py-1 rounded-xl">
+            <span key={index} className="bg-teal-600 px-2 mx-1 my-1 py-1 rounded-xl">
               {tagItem}
             </span>
           ))}
