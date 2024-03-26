5edb0092cbd81c695605c3aa403b37395ebbb6c2
Koustav Das <teamdecodeofficial@gmail.com>
2024-03-26 22:14:19 +0530

    commit

--- src/Resources/ResourceView.jsx
+++ src/Resources/ResourceView.jsx
@@ -9,7 +9,7 @@ const ResourceView = () => {
             <h3 className="titleHead mt-7">Resources.</h3>
             <p className="text-center text-md font-sans text-gray-500 mb-7">All the tools you need to better manage your finances</p>
             <div className="flex flex-col md:flex-row">
-                <div className="w-3/5 lg:w-4/5 xl:w-3/5 mx-auto"><Link to={"/resources"}><img className="w-3/5 md:w-4/5 xl:w-3/5 mx-auto select-none" src={getUrl("GroMarket-Resources-Page.png")} alt="GroMarket Resources" /></Link></div>
+                <div className="w-4/5 lg:w-3/4 xl:w-3/5 mx-auto"><Link to={"/resources"}><img className="w-3/5 md:w-4/5 xl:w-3/5 mx-auto select-none" src={getUrl("GroMarket-Resources-Page.png")} alt="GroMarket Resources" /></Link></div>
                 <ResourceList type={"full"} />
             </div>
         </section>
