c8a88a321a4ffdd5f6ec66a2c30929caf4587397
Koustav Das <contactkoustavdas@gmail.com>
2024-04-23 22:20:38 +0530

    nifty

--- src/Stocks/StocksPosts/Nifty50.jsx
+++ src/Stocks/StocksPosts/Nifty50.jsx
@@ -1,49 +1,50 @@
 import StockTemplate from '../StockTemplate/StockTemplate';
 const Nifty50 = () => {
 
-    const date = "22nd April, 2024";
+    const date = "23nd April, 2024";
     const stock = {
         name: "Nifty 50",
         nse: "NIFTY",
-        price: 22336.40,
-        change: 189.40,
-        return: 0.86,
-        open: 22336.90,
-        high: 22375.65,
-        low: 22198.15,
-        monthchange: 1.5,
-        threemonthchange: 3.5,
-        sma5: [22179.9, 1],
-        sma20: [22313.2, 1],
-        sma200: [20655.4, 6],
-        ema5: [22254.4, 3],
-        ema50: [22119.3, 4],
-        ema200: [20806.8, 6],
-        divident: 1.20,
-        pe: 22.7,
-        pb: 3.88,
-        mcap: 17789022,
-        cagr1yr: 26.6,
-        cagr5yr: 14.0,
-        macd: 32.4,
-        r1: 22408.7,
-        r2: 22480.9,
-        r3: 22586.2,
-        s1: 22231.2,
-        s2: 22125.9,
-        s3: 22053.7,
+        price: 22368.00,
+        change: 31.60,
+        return: 0.14,
+        open: 22447.05,
+        high: 22447.55,
+        low: 22349.45,
+        monthchange: 1.2,
+        threemonthchange: 3.7,
+        sma5: [22199.0, 1],
+        sma20: [22339.6, 1],
+        sma200: [20671.3, 6],
+        ema5: [22292.2, 6],
+        ema50: [22129.0, 5],
+        ema200: [20822.4, 5],
+        divident: 1.19,
+        pe: 22.8,
+        pb: 3.92,
+        mcap: 17950523,
+        cagr1yr: 26.7,
+        cagr5yr: 13.8,
+        macd: 34.5,
+        r1: 22427.2,
+        r2: 22486.4,
+        r3: 22525.3,
+        s1: 22329.1,
+        s2: 22290.2,
+        s3: 22231.0,
     };
     const link = "nifty50-analysis";
     const content = <ul>
-        <li>Bharat Petroleum Corporation Ltd. was a strong performer of the day showing a 3.01% gain, followed by Tata Consumer Products Ltd. with a 2.89% gain. Infosys Ltd. showed a gain of 1.48% for the day and ICICI Bank Ltd. with a 1.94% gain. 
+        <li>Bharti Airtel Ltd. is showing promising results with a 3.45% gain for the day and 8.6% gain over 1 Month. The stock shows can be traded with First Resistance at ₹1361.5 and First Support ₹1312.6. 
         </li>
-        <li>HDFC Bank was expected to rise but showed a drop of -1.25% since the opening of the market pulling the NIFTY 50 heavily down. 
+        <li>Maruti Suzuki India Ltd. was a Strong Performer of the day with a gain of ₹210.75 (1.65%). The stock showed a gain of 30.3% over 3 Months</li>
+        <li>HDFC Bank Ltd. showed a drop of -0.30% today. Yesterday it closed at a drop of -1.25%. The stock has strong upside potential.
         </li>
         <li>The day&#39;s high near the closing price suggests a potential &quot;Buyers Zone,&quot; indicating increased buying interest.
         </li>
-        <li>India&#39;s Reliance Industries reported fourth-quarter profit above analysts&#39; estimates on Monday, boosted by strength in its mainstay oil to chemicals (O2C) segment. There is an expected uptrend momentum for Reliance Industries Ltd. 
+        <li>Reliance Industries reported fourth-quarter profit above analysts&#39; estimates on Monday and a gain was expected but initially Reliance Industries showed a high of ₹3,025 but then dropped finally settling at ₹2,919 (-1.39%). The YoY Quaterly Profit Growth for the company is -1.34%.
         </li>
-        <li>For Short Term Investors, the market is Very Bearish. For Medium Term Investors the market is Neutral. For Long Term Investors the market is Bullish. Price is Near 52W High - a growth of 26.7% over 1 Year.
+        <li>For Short Term Investors, the market is Very Bearish. For Medium Term Investors the market is Neutral. For Long Term Investors the market is Bullish. Price is Near 52W High - a growth of 26.9% over 1 Year.
         </li>
     </ul>;
 
